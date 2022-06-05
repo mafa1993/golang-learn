d0b334469a9d9e45f9f7823458f93d4130c46a6b
Your Name <you@example.com>
2022-06-05 21:04:43 +0800

    切片基础

--- arrays.go
+++ arrays.go
@@ -29,7 +29,7 @@ func main() {
 	fmt.Println("改变前", arr2)
 	changeArr(&arr2)
 	fmt.Println("函数外arr", arr2)
-
+	slice()
 }
 
 /**
@@ -38,4 +38,36 @@ func main() {
 func changeArr(arr *[3]int) {
 	arr[0] = 100
 	fmt.Println("函数内", arr)
+}
+
+// 切片
+func slice() {
+	a := [...]int{1, 2, 3, 4, 5}
+	s := a[1:3]        // 切片，如果改变s的值，a会跟着变，s为a的一个视图, s为a某一部分的引用
+	fmt.Println(s)     // 包含2，不包含4，打印出2,3
+	fmt.Println(a[1:]) // 从位置1到最后
+	fmt.Println(a[:3]) // 从头到位置3
+	fmt.Println(a[:])  // 全部
+	s[0] = 1000        // 修改s，a也会变，将s传给函数进行修改，为引用传值，s和a都会变化
+	a[2] = 99          // 修改a，s也会变
+	fmt.Println("s", s)
+	fmt.Println("a", a)
+
+	// 切片可以reslice
+	s3 := a[1:4]
+	s3 = s3[1:2] // 从上一个s3中取1:2
+	fmt.Println(s3)
+
+	// 切面扩展
+	arr := [...]int{1, 2, 3, 4, 5}
+	s1 := arr[2:4]
+	fmt.Println("s1", s1)
+	s2 := s1[1:3]
+	fmt.Println(s2) // 4，5 对应的为S1的4，s1中没有5，去arr中找，取到4,5  如果超出了arr的长度，会报错，此为切片的扩展
+	// len(切片的长度) cap（从切片开头到arr最后一个元素的长度），不超过cap都可以扩展
+
+	// s[i] 不可以超越len[s]  向后扩展不可超过cap(s)
+
+	var ab []int
+	fmt.Println(cap(ab))
 }
